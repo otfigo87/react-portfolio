3b45aa50588296a7193c5d6871dd2a9dd8f1b550
Otmane Aatik <aaotmane@gmail.com>
2021-10-11 13:08:16 -0400

    About us

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutSection from '../components/AboutSection';
+
+function AboutUs() {
+    return (
+        <AboutSection/>
+            
+        
+    )
+}
+
+export default AboutUs

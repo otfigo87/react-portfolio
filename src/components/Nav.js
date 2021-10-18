fc551c825ec662b72ce40537816aff044f788249
Otmane Aatik <aaotmane@gmail.com>
2021-10-18 15:42:04 -0400

    nav animation line

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import styled  from 'styled-components';
 import {Link} from 'react-router-dom';
+import {motion} from 'framer-motion';
+import {useLocation} from 'react-router-dom';
 
 function Nav() {
+    const {pathname} = useLocation();
     return (
         <StyledNav>
            <h1>
@@ -11,12 +14,24 @@ function Nav() {
            <ul>
                <li>
                    <Link to="/">1. About Us</Link>
+                   <Line 
+                    initial={{ width: "0%"}}
+                    animate={{ width: pathname === "/" ? "50%" : "0%"}}
+                    transition={{duration:0.75}}/>
                </li>
                <li>
                    <Link to="work">2. Our Work </Link>
+                   <Line 
+                    initial={{ width: "0%"}}
+                    animate={{ width: pathname === "/work" ? "50%" : "0%"}}
+                    transition={{duration:0.75}}/>
                </li>
                <li>
                    <Link to="contactus">3. Contact Us</Link>
+                   <Line 
+                    initial={{ width: "0%"}}
+                    animate={{ width: pathname === "/contactus" ? "50%" : "0%"}}
+                    transition={{duration:0.75}}/>
                </li>
            </ul>
         </StyledNav>
@@ -64,5 +79,17 @@ const StyledNav = styled.nav`
        }
     }
 `;
+
+const Line = styled(motion.div)`
+  height: 0.3rem;
+  background-color:#23d997;
+  width: 0%;
+  position: absolute;
+  bottom: -80%;
+  left: 60%;
+  @media (max-width: 1300px) {
+      left: 0%;
+  }
+  `
 
 export default Nav
